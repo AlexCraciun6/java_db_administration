3b5057dffbbf5166fce1c03461906ecbfffde050
Craciun Alexandru <alexcraciun03@gmail.com>
2023-12-06 16:46:20 +0200

    primul post request

--- src/main/java/com/example/oracle_test/HomePageController.java
+++ src/main/java/com/example/oracle_test/HomePageController.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +13,50 @@ public class HomePageController {
     @Autowired
     public JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/")
-    public String greet(Model model){
-        model.addAttribute("something", "this is coming from controller");
+    public class Ending{
+        private String ending;
 
-        List<Furnizor> furnizori_3a = Interogari.getInterogare3a(jdbcTemplate, 8);
+        public String getEnding() {
+            return ending;
+        }
+
+        public void setEnding(String ending) {
+            this.ending = ending;
+        }
+
+    }
+
+    @GetMapping("/get_data_3a")
+    public String getData3a(Model model){
+
+
+
+        model.addAttribute("ending", new Ending());
+        return "get_data_3a";
+    }
+
+    @PostMapping("/interogare3a")
+    public String interogare3a(@ModelAttribute Ending ending, Model model){
+
+        List<Furnizor> furnizori_3a = Interogari.getInterogare3a(jdbcTemplate, Integer.parseInt(ending.getEnding()));
 
         model.addAttribute("furnizori_3a", furnizori_3a);
+        return "interogare_3a";
+    }
+
+    @GetMapping("/interogare3b")
+    public String interogare3b(Model model){
+        List<Comanda> comenzi_3b = Interogari.getInterogare3b(jdbcTemplate);
+
+        model.addAttribute("comenzi_3b", comenzi_3b);
+        return "interogare_3b";
+    }
+
+
+
+    @GetMapping
+    public String getHomePage(Model model){
+        model.addAttribute("something", "this is coming from controller");
         return "index";
     }
 
